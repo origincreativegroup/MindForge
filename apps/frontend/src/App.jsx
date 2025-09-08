731e98735895c9345770610b315d90d60dc9e681
origincreativegroup <jstasdesign@gmail.com>
2025-09-07 22:15:33 -0500

    Simplify dashboard entrypoint

--- apps/frontend/src/App.jsx
+++ apps/frontend/src/App.jsx
@@ -1,5 +1,14 @@
+import React from 'react';
+import BusinessDashboard from './components/BusinessDashboard.jsx';
 
+function App() {
+  return (
+    <div className="app">
+      <main>
+        <BusinessDashboard />
       </main>
     </div>
   );
 }
+
+export default App;
